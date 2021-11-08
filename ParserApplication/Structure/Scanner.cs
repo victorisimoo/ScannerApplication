cd9bf93d1f243b5cb1dcb92837596fb64be04ad8
sophiasiguere <sophia.siguere@gmail.com>
2021-11-07 20:11:05 -0600

    Analizador lexico Completo

--- ParserApplication/Structure/Scanner.cs
+++ ParserApplication/Structure/Scanner.cs
@@ -47,7 +47,8 @@ public class Scanner {
                             result.Value += peek.ToString();
                             _index++;
                             peek = _regexp [_index];
-                            while (char.IsLetterOrDigit(peek)) {
+                            while (char.IsWhiteSpace(peek)|| char.IsLetterOrDigit(peek) || peek >= 33 && peek <= 38 || peek >= 40 && peek <= 47 || peek >= 58 && peek <= 63 || peek >= 123 && peek <= 126 || peek >= 123 && peek <= 126 || peek >= 93 && peek <= 95 || peek == '[')
+                            {
                                 tokenFound = true;
                                 result.Tag = TokenType.term;
                                 result.Value += peek.ToString();
@@ -56,7 +57,8 @@ public class Scanner {
                             }
                             if ((char)TokenType.apostrofe == peek) {
                                 result.Value += peek.ToString();
-                            } else { MessageBox.Show("Error de lexema"); }
+                            } 
+                            else { MessageBox.Show("Error de lexema"); }
                             break;
                         case (char)TokenType.puntoycoma:
                         case (char)TokenType.igual:
@@ -86,7 +88,7 @@ public class Scanner {
                             result.Value += peek.ToString();
                             _index++;
                             peek = _regexp [_index];
-                            while (char.IsLetterOrDigit(peek)) {
+                            while (char.IsWhiteSpace(peek) || char.IsLetterOrDigit(peek) || peek >= 33 && peek <= 38 || peek >= 40 && peek <= 47 || peek >= 58 && peek <= 63 || peek >= 123 && peek <= 126 || peek >= 123 && peek <= 126 || peek >= 93 && peek <= 95 || peek == '[') {
                                 tokenFound = true;
                                 result.Tag = TokenType.term;
                                 result.Value += peek.ToString();
