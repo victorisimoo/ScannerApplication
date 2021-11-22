55bba87036117174a36a7a911fd28d3ae5998d27
JUAN SEBASTIAN MEJIA HERNANDEZ <jmejiah@correo.url.edu.gt>
2021-11-22 16:32:19 -0600

    Lookahead implemented

--- ParserApplication/LALR/TableItem.cs
+++ ParserApplication/LALR/TableItem.cs
@@ -14,7 +14,6 @@ namespace ParserApplication.LALR
         public Dictionary<string, int> Shifts = new Dictionary<string, int>();
         public Dictionary<string, int> Gotos = new Dictionary<string, int>();
         public List<ListadeTokens> EstadoProduction = new List<ListadeTokens>();
-
         public TableItem(List<ListadeTokens> Production,string entry,int anterior,TokenType type) {
             EstadoProduction = new List<ListadeTokens>(Production);
             valuesent = entry;
@@ -22,6 +21,15 @@ namespace ParserApplication.LALR
             Typesent = type;
         }
 
-
+        public void SetLookAheads(List<FirstFollow> Follow) {
+            foreach (var item in EstadoProduction) {
+                foreach (var item2 in Follow)
+                {
+                    if (item.identifier == item2.token.Value) {
+                        item.Lookaheads.AddRange(item2.lista);
+                    }
+                }
+            }
+        }
     }
 }
